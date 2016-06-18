01467f8eb44b27a3c8d6e78bb83a11afcc4fd63a
Miguel Michelson <miguelmichelson@gmail.com>
2016-06-17 21:59:03 -0300

    v4 intent

--- Gemfile
+++ Gemfile
@@ -2,22 +2,33 @@
 # the following line to use "https"
 source 'http://rubygems.org'
 
-#gem "therubyracer"
-gem "middleman", "~>3.1.4"
+##gem "therubyracer"
+#gem "middleman", "~>3.1.4"
+#
+## Live-reloading plugin
+#gem "middleman-livereload", "~> 3.1.0"
+#gem 'middleman-gh-pages'
+## For faster file watcher updates on Windows:
+#gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
 
-# Live-reloading plugin
-gem "middleman-livereload", "~> 3.1.0"
-gem 'middleman-gh-pages'
-# For faster file watcher updates on Windows:
-gem "wdm", "~> 0.1.0", :platforms => [:mswin, :mingw]
 gem "github-markup"
 gem "redcarpet"
 gem "sinatra"
 gem "pry"
 
-gemspec
+# For faster file watcher updates on Windows:
+gem 'wdm', '~> 0.1.0', platforms: [:mswin, :mingw]
 
-# Cross-templating language block fix for Ruby 1.8
-platforms :mri_18 do
-  gem "ruby18_source_location"
-end
+# Windows does not come with time zone data
+gem 'tzinfo-data', platforms: [:mswin, :mingw, :jruby]
+
+gem "middleman-sprockets", github: "middleman/middleman-sprockets"
+
+# Middleman Gems
+gem 'middleman', '>= 4.0.0'
+gem 'middleman-livereload'
+
+gem 'middleman-gh-pages'
+
+
+gemspec
--- config.rb
+++ config.rb
@@ -1,11 +1,11 @@
 
 class DistBuilder < Middleman::Extension
-
   def initialize(app, options_hash={}, &block)
     super
 
     app.after_build do |builder|
       app.config[:dist_dir] = "dist"
+      
       FileUtils.rm_rf Dir.glob("#{app.config[:dist_dir]}/*")
       base_dist_path   = app.config[:dist_dir]
       dist_fonts_path  = File.join(base_dist_path, "fonts", "dante")
@@ -13,9 +13,7 @@ class DistBuilder < Middleman::Extension
       dist_js_path     = File.join(base_dist_path, "js")
       dist_css_path    = File.join(base_dist_path, "css")
 
-      #FileUtils.rm_rf base_dist_path
-
-      builder.say_status :dist_builder, "generating dist files"
+      puts "generating dist files"
 
       fonts_dir = File.join(app.config[:build_dir], app.config[:fonts_dir])
       FileUtils.mkdir_p dist_fonts_path
@@ -25,33 +23,25 @@ class DistBuilder < Middleman::Extension
       FileUtils.mkdir_p dist_images_path
       FileUtils.cp_r "#{images_dir}/dante/.", dist_images_path, :verbose => true
 
-
       js_dir = File.join(app.config[:build_dir], app.config[:js_dir])
       FileUtils.mkdir_p dist_js_path
       FileUtils.cp "#{js_dir}/dante-editor.js", dist_js_path, :verbose => true
-
 
       css_dir = File.join(app.config[:build_dir], app.config[:css_dir])
       FileUtils.mkdir_p dist_css_path
       FileUtils.cp "#{css_dir}/dante-editor.css", dist_css_path, :verbose => true
 
-      builder.say_status :dist_builder, "dist complete"
+      puts "dist complete"
 
     end
   end
-
 end
 
 ::Middleman::Extensions.register(:dist_builder, DistBuilder)
 
-
-
 set :css_dir, 'assets/stylesheets'
-
 set :js_dir, 'assets/javascripts'
-
 set :images_dir, 'assets/images'
-
 set :fonts_dir, 'assets/fonts'
 
 #set :markdown_engine, :redcarpet
@@ -60,8 +50,11 @@ set :markdown_engine, :kramdown
 page "/tests/*", :layout => "spec"
 page '/api/*', :content_type => 'application/json', layout: false
 
-sprockets.append_path File.join "#{root}", "bower_components"
+activate :sprockets
 
+after_configuration do
+  sprockets.append_path File.join "#{root}", "bower_components"
+end
 
 # Build-specific configuration
 configure :build do
@@ -74,13 +67,14 @@ configure :build do
   # Enable cache buster
   # activate :asset_hash
 
-  compass_config do |config|
-    config.line_comments = false
-  end
+  #compass_config do |config|
+  #  config.line_comments = false
+  #end
 
   activate :relative_assets
   set :relative_links, true
   activate :dist_builder
+
 
   # Use relative URLs
   # activate :relative_assets
